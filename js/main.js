b84ad19a4dca1da9fc89b1380faeb733f7afa7df
lenocheklisichka <47819860+lenocheklisichka@users.noreply.github.com>
2019-12-04 22:14:39 +0300

    добавила js

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(function(){
+	$('.theme').hide(2000).show(2000);
+	 $('.box-item > .item').hide(3000).show(3000);
+	 $('.box-template > .template > .text-template').hide(3000).show(3000);
+});
+
+// $(function(){
+// 	var w = $('.btn-one').width();
+// 	alert(w);
+// 	var h = $('.btn-one').height();
+// 	alert(h);
+// })
+
+$(function(){
+	// $('.menu').fadeOut(4000).fadeIn(3000);
+    $('.menu').fadeTo(4000, 0.2).fadeTo(3000,1);
+});
+
+$(function(){
+   $('.box-psd').slideUp(2000).slideDown(2000);
+   $('.web-site').slideUp(2000).slideDown(2000);
+});
+
+$(function(){
+   $('.block').hide(2000).show(2000);
+   $('.block-just-one').fadeOut(4000).fadeIn(3000);
+   $('.box-lorem').fadeOut(4000).fadeIn(3000);
+   
+});
